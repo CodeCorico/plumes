e66c0cf01dbde50b6f60f7a3f413fc2fa45cef74
Xavier Boubert <xavier@boubert.fr>
2015-06-18 17:09:25 +0200

    fix(layout bootstrap): get partials from initial body
    This fix variables deleted from partials passed in the first view template

--- features/layout/js/layout-bootstrap.js
+++ features/layout/js/layout-bootstrap.js
@@ -3,12 +3,28 @@
 
   window.Ractive.bootstrap = function(callback) {
     $(function() {
-      var $cleaner = $('<div />').append($('body').html());
+      var partials = {},
+          $cleaner = $('<div />').append($('body').html());
+
       $cleaner.find('script[type="text/javascript"]').remove();
+
+      var $rvPartials = $cleaner.find('rv-partial');
+
+      $rvPartials.each(function(i, rvPartial) {
+        var src = rvPartial.getAttribute('src') || false,
+            target = rvPartial.getAttribute('target');
+
+        if (!src && target) {
+          partials[target] = $.trim(rvPartial.innerHTML);
+        }
+      });
+
+      $rvPartials.remove();
 
       var page = new window.Ractive({
         el: 'body',
-        template: $cleaner.html()
+        template: $.trim($cleaner.html()),
+        partials: partials
       });
 
       if (callback) {
