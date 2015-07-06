a16614515a26694122a5551ed8e1daa1647cabe7
Xavier Boubert <xavier@boubert.fr>
2015-07-06 15:21:30 +0200

    feat(dropdown-title): add addTitle() and removeTitle() methods

--- features/dropdowns/js/dropdown-title.js
+++ features/dropdowns/js/dropdown-title.js
@@ -102,6 +102,39 @@
 
     dropdownTitle.selectApp = function(name, fireFunc, callback) {
       _select(name, fireFunc, callback);
+
+      return dropdownTitle;
+    };
+
+    dropdownTitle.addTitle = function(title, indexOrPosition) {
+      var titles = dropdownTitle.get('titles');
+
+      indexOrPosition = typeof indexOrPosition == 'undefined' ? titles.length : indexOrPosition;
+
+      if (typeof indexOrPosition == 'string') {
+        indexOrPosition = indexOrPosition == 'top' ? 0 : titles.length;
+      }
+
+      titles.splice(indexOrPosition, 0, title);
+
+      dropdownTitle.set('titles', titles);
+
+      return dropdownTitle;
+    };
+
+    dropdownTitle.removeTitle = function(name) {
+      var titles = dropdownTitle.get('titles');
+
+      for (var i = 0; i < titles.length; i++) {
+        if (titles[i].name == name) {
+          titles.splice(i, 1);
+          break;
+        }
+      }
+
+      dropdownTitle.set('titles', titles);
+
+      return dropdownTitle;
     };
 
     done();
