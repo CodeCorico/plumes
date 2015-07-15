31bb0b942584641e65e98b87c04c40b033fbca45
Xavier Boubert <xavier@boubert.fr>
2015-07-15 12:05:18 +0200

    feat(login): create the forget password feature

--- features/login/js/login.js
+++ features/login/js/login.js
@@ -4,8 +4,13 @@
   window.Ractive.controller('login', function(component, data, el, config, done) {
 
     var _$el = {
-          window: $(window)
+          window: $(window),
+          login: null
         };
+
+    data.useforgot = typeof data.useforgot == 'undefined' ? true : data.useforgot;
+    data.useforgot = data.useforgot == 'true' ? true : data.useforgot;
+    data.useforgot = data.useforgot == 'false' ? false : data.useforgot;
 
     data.texts = {};
 
@@ -17,8 +22,25 @@
     });
 
     data.avatar = !data.avatar ? 'null' : data.avatar;
+    data.inForgot = false;
+
     data.texts.username = typeof data.texts.username == 'undefined' ? 'Name' : data.texts.username;
+    data.usernamePlaceholder = data.texts.username;
+    data.texts.code = typeof data.texts.code == 'undefined' ? 'Code' : data.texts.code;
     data.texts.userpassword = typeof data.texts.userpassword == 'undefined' ? 'Password' : data.texts.userpassword;
+    data.userpasswordPlaceholder = data.texts.userpassword;
+    data.texts.userpasswordconfirm = typeof data.texts.userpasswordconfirm == 'undefined' ? 'Confirm password' : data.texts.userpasswordconfirm;
+    data.texts.forgotlink = typeof data.texts.forgotlink == 'undefined' ? 'I forgot my password' : data.texts.forgotlink;
+    data.texts.login = typeof data.texts.login == 'undefined' ? 'Back to login' : data.texts.login;
+    data.texts.forgottext = typeof data.texts.forgottext == 'undefined' ?
+      'Submit your email and press <span class="key">ENTER</span> to receive a validation code in your mailbox.' :
+      data.texts.forgottext;
+    data.texts.forgotcodetext = typeof data.texts.forgotcodetext == 'undefined' ?
+      'Use your <strong>validation code</strong> and press <span class="key">ENTER</span> to get the possibility to change your password.' :
+      data.texts.forgotcodetext;
+    data.texts.forgotpasswordtext = typeof data.texts.forgotpasswordtext == 'undefined' ?
+      'Enter your new password twice (to confirm) and press <span class="key">ENTER</span> to change your password.' :
+      data.texts.forgotpasswordtext;
 
     function _refresh() {
       var messageHeight = _$el.message.outerHeight(true);
@@ -33,8 +55,110 @@
       }, data)
     });
 
-    Login.on('passwordFocus', function() {
-      Login.fire('usernameChanged', Login.get('username'));
+    _$el.login = $(Login.el);
+
+    function _focusName() {
+      _$el.login.find('.name').focus();
+    }
+
+    function _modal(message, type, clearUsername, clearUserpassword, callback) {
+      Login.set('inModal', true);
+      Login.set('message', message);
+      Login.set('messageType', type);
+      Login.set('storyboardMessage', 'sb-message-1');
+
+      setTimeout(function() {
+        if (clearUsername) {
+          Login.set('username', '');
+        }
+        if (clearUserpassword) {
+          Login.set('userpassword', '');
+        }
+
+        if (type == 'success') {
+          Login.set('usernamePlaceholder', data.texts.username);
+          Login.set('userpasswordPlaceholder', data.texts.userpassword);
+        }
+
+        Login.set('storyboardMessage', 'sb-message-2');
+        setTimeout(function() {
+          Login.set('storyboardMessage', 'sb-message-3');
+          setTimeout(function() {
+            Login.set('storyboardMessage', 'sb-message-4');
+            setTimeout(function() {
+              Login.set('storyboardError', null);
+              Login.set('inModal', false);
+
+              if (callback) {
+                callback();
+              }
+            }, 350);
+          }, 250);
+        }, 3000);
+      }, 250);
+    }
+
+    function _error(message, clearUsername, clearUserpassword, callback) {
+      _modal(message, 'error', clearUsername, clearUserpassword, callback);
+    }
+
+    function _success(message, callback) {
+      _modal(message, 'success', true, true, callback);
+    }
+
+    function _submit() {
+      if (Login.get('inForgotPassword')) {
+        Login.fire('forgotPassword', {
+          password: Login.get('username'),
+          confirmPassword: Login.get('userpassword')
+        });
+      }
+      else if (Login.get('inForgotCode')) {
+        Login.fire('forgotCode', {
+          code: Login.get('username')
+        });
+      }
+      else if (Login.get('inForgot')) {
+        Login.fire('forgot', {
+          username: Login.get('username')
+        });
+      }
+      else {
+        Login.fire('login', {
+          username: Login.get('username'),
+          userpassword: Login.get('userpassword')
+        });
+      }
+    }
+
+    _$el.login.find('.login-form-content .input').keypress(function(event) {
+      // enter
+      if (event.which == 13) {
+        _submit();
+
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    });
+
+    var lastObserveUsernameDate = new Date();
+
+    Login.observe('username', function() {
+      if (Login.get('inForgotCode') || Login.get('inForgotPassword')) {
+        return;
+      }
+
+      lastObserveUsernameDate = new Date();
+
+      var username = Login.get('username');
+
+      setTimeout(function() {
+        if (new Date().getTime() - lastObserveUsernameDate.getTime() >= 1000) {
+          Login.fire('username', {
+            username: username
+          });
+        }
+      }, 1000);
     });
 
     Login.observe('avatar', function(value, oldValue) {
@@ -103,15 +227,6 @@
 
     Login.on('close', _close);
 
-    Login.on('submit', function(event) {
-      Login.fire('login', {
-        username: Login.get('username'),
-        userpassword: Login.get('userpassword')
-      });
-
-      event.original.preventDefault();
-    });
-
     Login.on('error', function(args) {
       args = $.extend(true, {
         message: 'You have an error in your credentials.',
@@ -119,28 +234,108 @@
         clearUserpassword: true
       }, args || {});
 
-      Login.set('error', args.message);
-      Login.set('storyboardError', 'sb-error-1');
+      _error(args.message, args.clearUsername, args.clearUserpassword, _focusName);
+    });
 
-      setTimeout(function() {
-        if (args.clearUsername) {
-          Login.set('username', '');
-        }
-        if (args.clearUserpassword) {
-          Login.set('userpassword', '');
-        }
+    Login.on('displayForgot', function() {
+      Login.set('inForgot', true);
+      Login.fire('enterForgot');
 
-        Login.set('storyboardError', 'sb-error-2');
-        setTimeout(function() {
-          Login.set('storyboardError', 'sb-error-3');
-          setTimeout(function() {
-            Login.set('storyboardError', 'sb-error-4');
-            setTimeout(function() {
-              Login.set('storyboardError', null);
-            }, 350);
-          }, 250);
-        }, 3000);
-      }, 250);
+      setTimeout(_focusName);
+    });
+
+    Login.on('displayLogin', function() {
+      var inForgot = Login.get('inForgot'),
+          inForgotCode = Login.get('inForgotCode'),
+          inForgotPassword = Login.get('inForgotPassword');
+
+      if (inForgotCode || inForgotPassword) {
+        Login.set('username', '');
+        Login.set('userpassword', '');
+      }
+
+      Login.set('inForgot', false);
+      Login.set('inForgotCode', false);
+      Login.set('inForgotPassword', false);
+      Login.set('usernamePlaceholder', data.texts.username);
+      Login.set('userpasswordPlaceholder', data.texts.userpassword);
+
+      if (inForgotCode) {
+        Login.fire('exitForgotCode');
+      }
+
+      if (inForgotPassword) {
+        Login.fire('exitForgotPassword');
+      }
+
+      if (inForgot) {
+        Login.fire('exitForgot');
+      }
+
+      setTimeout(_focusName);
+    });
+
+    Login.on('forgotSuccess', function() {
+      Login.set('inForgotCode', true);
+      Login.set('username', '');
+      Login.set('usernamePlaceholder', data.texts.code);
+
+      Login.fire('enterForgotCode');
+
+      setTimeout(_focusName);
+    });
+
+    Login.on('forgotError', function(args) {
+      args = $.extend(true, {
+        message: 'This email address doesn\'t exist.',
+        clearUsername: false,
+        clearUserpassword: false
+      }, args || {});
+
+      _error(args.message, args.clearUsername, args.clearUserpassword, _focusName);
+    });
+
+    Login.on('forgotCodeSuccess', function() {
+      Login.set('inForgotCode', false);
+      Login.set('inForgotPassword', true);
+      Login.set('username', '');
+      Login.set('usernamePlaceholder', data.texts.userpassword);
+      Login.set('userpassword', '');
+      Login.set('userpasswordPlaceholder', data.texts.userpasswordconfirm);
+
+      Login.fire('enterForgotPassword');
+
+      setTimeout(_focusName);
+    });
+
+    Login.on('forgotCodeError', function(args) {
+      args = $.extend(true, {
+        message: 'Code invalid.',
+        clearUsername: true,
+        clearUserpassword: false
+      }, args || {});
+
+      _error(args.message, args.clearUsername, args.clearUserpassword, _focusName);
+    });
+
+    Login.on('forgotPasswordSuccess', function(args) {
+      args = $.extend(true, {
+        message: 'New password registered.'
+      }, args || {});
+
+      _success(args.message, function() {
+        Login.fire('displayLogin');
+      });
+    });
+
+    Login.on('forgotPasswordError', function(args) {
+      args = $.extend(true, {
+        message: 'Passwords are invalid.',
+        clearUsername: true,
+        clearUserpassword: true
+      }, args || {});
+
+      _error(args.message, args.clearUsername, args.clearUserpassword, _focusName);
     });
 
     function _resize() {
@@ -150,7 +345,7 @@
 
       var storyboard = Login.get('storyboard');
 
-      if (storyboard == 'sb-show-2' || storyboard == 'sb-show-3') {
+      if (storyboard == 'sb-show-2' || storyboard == 'sb-show-3' || storyboard == 'sb-show-4') {
         _refresh();
       }
     }
@@ -185,7 +380,8 @@
               Login.set('storyboard', 'sb-show-3');
 
               setTimeout(function() {
-                $(Login.el).find(!data.username ? '.name' : '.password').focus();
+                _$el.login.find(!data.username ? '.name' : '.password').focus();
+                Login.set('storyboard', 'sb-show-4');
               }, 450);
 
             }, 450);
